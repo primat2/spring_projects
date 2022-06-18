186627bb61cdcfca0fc9374d18e72ca19cb528d4
Артем Гавшин <cerebrum90@gmail.com>
2022-06-18 14:03:00 +0500

    Simple auth

---
+++ spring_auth_learn/src/main/java/com/example/spring_auth_learn/simple_auth/SpringAuthLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_auth_learn.simple_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthLearnApplication.class, args);
+    }
+
+}
---
+++ spring_auth_learn/src/test/java/com/example/spring_auth_learn/SpringAuthLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_auth_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
